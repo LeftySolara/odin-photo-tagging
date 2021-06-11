431c42aca0e301a770ed94d94d77ca7ea51ce6f8
Jalen Adams <jalen@jalenkadams.me>
2021-06-10 21:09:24 -0400

    Add: buttons for dropdown menu

--- src/components/DropdownMenu/DropdownMenu.js
+++ src/components/DropdownMenu/DropdownMenu.js
@@ -3,11 +3,26 @@ import styled from 'styled-components';
 
 const StyledDropdownMenu = styled.ul`
   position: absolute;
+  display: flex;
+  flex-flow: column nowrap;
+  margin: 0px;
+  padding: 0px;
   left: ${(props) => props.point.x}px;
   top: ${(props) => props.point.y}px;
   list-style: none;
-  background-color: white;
-  color: black;
+  background-color: rgba(87, 117, 144, 0);
+  border: 1px solid rgba(87, 117, 144, 0);
+  border-radius: 7px;
+`;
+
+const StyledButton = styled.button`
+  width: 100%;
+  margin-top: 3px;
+  background-color: #f94144;
+  color: white;
+  border: 3px solid white;
+  border-radius: 10px;
+  font-weight: bold;
 `;
 
 /**
@@ -22,7 +37,19 @@ const StyledDropdownMenu = styled.ul`
 const DropdownMenu = (props) => {
   const { point, menuItems } = props;
 
-  return <StyledDropdownMenu point={point}>{menuItems}</StyledDropdownMenu>;
+  const buttons = menuItems.map((item) => {
+    return (
+      <StyledButton type="button" onClick={item.onClick}>
+        {item.name}
+      </StyledButton>
+    );
+  });
+
+  return (
+    <div>
+      <StyledDropdownMenu point={point}>{buttons}</StyledDropdownMenu>
+    </div>
+  );
 };
 
 export default DropdownMenu;
